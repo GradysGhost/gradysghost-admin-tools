c495005995d4c5214f9d530b991a1f9d545857fc
Ryan Jung <gradysghost@gmail.com>
2012-07-19 11:23:04 -0500

    Added MySQLTuner launcher.

---
+++ mysqltuner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Depends on unzip
+# Creates/deletes /tmp/mysqltuner
+
+mkdir /tmp/mysqltuner
+cd /tmp/mysqltuner
+curl https://nodeload.github.com/rackerhacker/MySQLTuner-perl/zipball/master > mysqltuner.zip
+unzip -j ./mysqltuner.zip > /dev/null
+perl ./mysqltuner.pl
