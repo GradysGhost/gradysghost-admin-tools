34783197f8d632eea1d9c1df00cc749c42db9c8f
Ryan Jung <ryan.jung@rackspace.com>
2013-01-22 14:01:06 -0600

    Updated procstats.sh for better RAM assessment

--- procstats.sh
+++ procstats.sh
@@ -4,11 +4,19 @@ PROCESS_NAME=httpd
 
 TOTAL=0;
 COUNT=0;
-for VAL in `ps aux | grep $PROCESS_NAME | awk '{print $4}'`
+for PID in `ps aux | grep $PROCESS_NAME | grep -v grep | awk '{print $2}'`
 do
-	TOTAL=$(echo "$TOTAL + $VAL" | bc)
-	COUNT=$(($COUNT + 1))
+	VAL=`pmap -d $PID | grep writeable/private | awk '{print $4}' | sed 's/K//'`
+    TOTAL=$(echo "$TOTAL + $VAL" | bc)
+    COUNT=$(($COUNT + 1))
 done
 MEAN=$(echo "$TOTAL / $COUNT" | bc)
-echo "Total memory consumption: $TOTAL%"
-echo "Average memory consumption per process: $MEAN%"
+TOTAL_MB=$(echo "$TOTAL / 1024" | bc)
+MEAN_MB=$(echo "$MEAN / 1024" | bc)
+echo "Process name: $PROCESS_NAME"
+echo "Total processes: $COUNT"
+echo "Total memory consumption: $TOTAL bytes"
+echo "        $TOTAL_MB MB"
+echo "Average memory consumption per process: $MEAN bytes"
+echo "        $MEAN_MB MB"
+
