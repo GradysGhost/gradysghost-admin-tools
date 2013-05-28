0197a16ec675f8c4d75229fd038f6e31232aa187
Ryan Jung <ryan.jung@rackspace.com>
2013-05-28 16:48:42 -0500

    Fixed the DB conversion script

--- bash/convert-myisam-to-innodb.sh
+++ bash/convert-myisam-to-innodb.sh
@@ -5,16 +5,17 @@ SCHEMA=$1
 mysql -Be "USE $SCHEMA;" &> /dev/null
 EXITCODE=$?
 if [ $EXITCODE -eq 0 ]; then
-	mysql information_schema --skip-column-names -Be 'USE `information_schema`; SELECT `table_name` FROM `TABLES` WHERE `table_schema` = "$SCHEMA" AND `engine`="MyISAM";' > /tmp/myisamdbs
-	while read TABLE; do
-		echo "Converting table $TABLE..."
-		mysql $SCHEMA -Be "ALTER TABLE $TABLE ENGINE=InnoDB;"
-		if [ $? -eq 0 ]; then
-			echo "Table $TABLE converted successfully."
-		else
-			echo "Could not convert table $TABLE."
-		fi
-	done < /tmp/myisamdbs
+        QUERY="USE `information_schema`; SELECT `table_name` FROM `TABLES` WHERE `table_schema` = \"$SCHEMA\" AND `engine`='MyISAM';"
+        mysql information_schema --skip-column-names -Be "$QUERY" > /tmp/myisamdbs
+        while read TABLE; do
+                echo "Converting table $TABLE..."
+                mysql $SCHEMA -Be "ALTER TABLE $TABLE ENGINE=InnoDB;"
+                if [ $? -eq 0 ]; then
+                        echo "Table $TABLE converted successfully."
+                else
+                        echo "Could not convert table $TABLE."
+                fi
+        done < /tmp/myisamdbs
 else
-	echo "Could not access schema $SCHEMA."
+        echo "Could not access schema $SCHEMA."
 fi
