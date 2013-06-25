7f16d584acc6688ee93fd186904290f07804f01d
Ryan Jung <GradysGhost@gmail.com>
2013-06-25 14:29:31 -0500

    Added script to test

---
+++ bash/get-mysql-data-size.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -Be "SELECT count(*) tables, concat(round(sum(table_rows)/1000000,2),'M') rows, concat(round(sum(data_length)/(1024*1024*1024),2),'G') data, concat(round(sum(index_length)/(1024*1024*1024),2),'G') idx, concat(round(sum(data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(sum(index_length)/sum(data_length),2) idxfrac FROM information_schema.TABLES\G"
