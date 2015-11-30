c56dea36eaf0bff5cb909a76bf26aed3f763ceb7
Ryan Jung <gradysghost@gmail.com>
2015-11-29 20:46:15 -0700

    Add compression support to db-backup.sh

--- bash/db-backup.sh
+++ bash/db-backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Script to create a mysql backup and delete old backups
  
-# Configuration - fill in the blanks
+# Configuration
 mysql_host=""
 mysql_username=""
 mysql_password=''
 backup_path=/backup
+compress=bzip2       # Set to "no" to disable, or set to a compression command like "bzip2"
 expired=30           #how many days before the backup directory will be removed
 
 mysql_database="$1"
@@ -15,6 +16,7 @@ if [ -z "$mysql_database" ]; then
 fi
  
 today=`date +%Y-%m-%d`
+now=`date +%H-%M`
  
 if [ "$mysql_database" == "--all-databases" ]; then
     backup_path="$backup_path/$today/000-full-db"
@@ -28,8 +30,11 @@ mkdir -p "$backup_path"
   -h "$mysql_host" \
   -u "$mysql_username" \
   --password="$mysql_password" \
-  "$mysql_database" > "$backup_path/`date +%H-%M`.sql"
+  "$mysql_database" > "$backup_path/$now.sql"
+
+if [ "$compress" != "no" ]; then
+    $compress "$backup_path/$now.sql"
+fi
 
 # Remove old folders
 find "$backup_path" -type d -mtime +$expired | xargs rm -Rf
-
